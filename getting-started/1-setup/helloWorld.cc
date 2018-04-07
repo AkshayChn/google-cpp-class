61b48c92352d318b8c7f66a618a75fa0d0c193b9
AkshayC <chnakshay@gmail.com>
2018-04-07 23:14:35 +0530

    Adding the previous init work

---
+++ getting-started/1-setup/helloWorld.cc
@@ -0,0 +1,11 @@
+// helloWorld.cc: Akshay C
+// Description: a program that prints the immortal saying "hello world!"
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello WOrld!" << endl;
+	return 0;
+}
