b196c5792b25481a37f9af2417a3d0b0a65cedef
AkshayC <chnakshay@gmail.com>
2018-04-10 22:24:56 +0530

    adding the file on strings

---
+++ getting-started/1-setup/10.0_string_test.cc
@@ -0,0 +1,12 @@
+// string_test.cc : Akshay C.
+// Desc: This is a dummy program where 
+// I play around with different strings.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str1 = "Hello world!";
+  cout << str1 << endl;
+  return 0;
+}
